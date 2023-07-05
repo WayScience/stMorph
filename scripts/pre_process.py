7a80069b575c7d10b82ed3a3bf540df8a389a06e
Dayna Olson <daynaolson@maple.ucdenver.pvt>
2023-07-05 09:35:00 -0600

    test

--- scripts/pre_process.py
+++ scripts/pre_process.py
@@ -2,79 +2,16 @@ import numpy as np
 import tifffile as tiff
 import pandas as pd
 import os
-from orig_tiff import OrigTiff
-import shutil
-import sys
-sys.path.append('CellSeg')
-from CellSeg import main
+import orig_tiff
 
-def cell_seg_prep(img:tiff, output_dir, numx:int,numy:int,channel_names):
-	'''Prepares image for CellSeg including tiling. Returns output_dir path that includes tiles and output subfolders.
-
-	inputs:
-	img -- str -- path to tiff image to crop
-	output_dir -- str -- path to directory one up of desired tiles and CellSeg output directories.
-	numx -- int -- number of tiles to divide image into on x-axis
-	numy -- number of tiles to divide image into on y-axis (for example, if numx=4 and numy=2 the image will be cropped into tiles by a 4x2 grid)
-	channel_names -- str -- path to text file that includes each image channel name as a line in order'''
+img = "/home/daynaolson/10x_prostate_run/Visium_FFPE_Human_Prostate_IF_image.tif"
+crop_tif_x = 4
+crop_tif_y = 2
 
 
-	if not os.path.exists(output_dir):
-		os.mkdir(output_dir)
-	if not os.path.exists("%s/tiles" % output_dir):
-		os.mkdir("%s/tiles" % output_dir)
-	if not os.path.exists("%s/output" % output_dir):
-		os.mkdir("%s/output" % output_dir)
 
-	tif = OrigTiff(img,numx,numy)
-	tif.crop_tif("%s/tiles" % output_dir)
-	shutil.copyfile(channel_names, "%s/channelNames.txt" % output_dir)
-	target = output_dir
-	return target
+def merge_coords():
 
-def run_cell_seg(img:tiff, output_dir, numx:int,numy:int,channel_names):
-	'''Prepares image for CellSeg and runs CellSeg using the submodule.
-
-	inputs:
-	img -- str -- path to tiff image to crop
-	output_dir -- str -- path to directory one up of desired tiles and CellSeg output directories.
-	numx -- int -- number of tiles to divide image into on x-axis
-	numy -- number of tiles to divide image into on y-axis (for example, if numx=4 and numy=2 the image will be cropped into tiles by a 4x2 grid)
-	channel_names -- str -- path to text file that includes each image channel name as a line in order'''
-
-	target = cell_seg_prep(img,output_dir,numx,numy,channel_names)
-	main.main(target)
-
-def merge_coords(coord_dir, file_names, xbounds:float, ybounds:float, x_name:str, y_name:str, output_dir: str):
-	'''Converts CellSeg-segmetned tile cell center coordinates to coordinates in original image. Returns path to csv that contains cell coordiantes for the entire image.
-
-	inputs:
-	coord_dir -- str -- path to directory that contains CellSeg coordinate output files.
-	files_names -- list of str -- list of coordinate file names, using the OrigTiff tile naming convention
-	xbounds -- list of float-- list of pixel x values for tile boundaries in terms of the entire image
-	ybounds -- list of float -- list of pixel y values for tile boundaries in terms of the entire image
-	x_name -- name of column in the coordinate file that contains cell center x coordinates in terms of the individual tile
-	y_name -- name of column in the coordinate file that contains cell center y coordinates in terms of the individual tile
-	output_dir -- str -- path to directory to output full coordinate file'''
-	isExist = os.path.exists(output_dir)
-	if not isExist:
-		os.makedirs(output_dir)
-	X = []
-	Y = []
-	for name in files:
-		data = pd.read_csv("%s/%s__statistics_growth2_uncomp.csv" % (coord_dir, file_name), usecols=[x_name,y_name])
-		x_offset = xbounds[int(name[3]-1)]
-		y_offset = ybounds[int(name[2]-1)]
-		x = np.array(data[x_name]) + x_offset
-		y = np.array(data[y_name]) + y_offset
-		X.append(x)
-		Y.append(y)
-	X_all = [item for sublist in X for item in sublist]
-	Y_all = [item for sublist in Y for item in sublist]
-	coords = np.array((X_all, Y_all))
-	path = "%s/cell_coords.npz" % output_dir
-	np.savez("%s/cell_coords.npz" % output_dir, coords=coords)
-	return path
 
 
 def crop_cell(img: np.array, x: int or float, y: int or float, box_size: int, idx: int, output_dir: str = "None") -> np.array:
@@ -101,43 +38,28 @@ def crop_cell(img: np.array, x: int or float, y: int or float, box_size: int, id
 	crop = img[:,ymin:ymax, xmin:xmax]
 
 	if output_dir != "None":
-		tiff.imwrite("%s/cell_%i.tif" % (output_dir, idx), crop, imagej=True)
+		tiff.imwrite("%s/cell_%i.tif" % (output_dir, cell_idx), crop, imagej=True)
 	return crop
 
-def img_to_cells(img: tiff, segs, box_size: int, output_dir: str):
+def img_to_cells(img: tiff, segs: csv, x_center_col: str y_center_col: str, box_size: int, output_dir: str):
 	'''Takes in pre-segmented multichannel, saves each cell as an individual image in tiff format of a specified box size, and returns list of cropped cell image arrays.
 
 		inputs:
 		img -- str -- path to original tiff image
-		segs -- csv -- path to array (npz) that contains segmentation information (particular x and y centers for each cell under "coords")
+		segs -- csv -- path to CSV that contains segmentation information (particular x and y centers for each cell)
+		x_center_col -- str -- name of csv column that contains x centers for each cell
+		y_center_col -- str -- name of csv column that contains y centers for each cell
 		box_size -- int -- desired image size for cell crops
 		output_dir -- str -- desired output directory path'''
-	segs = np.load(segs)
-	segs = segs["coords"]
+	segs = pd.read_csv(segs, usecols=[x_center_col, y_center_col])
 	img = tiff.imread(img)
-	#cell_imgs = []
-	for i in range(segs.shape[1]):
-		crop_cell(img, segs[0,i], segs[1,i], box_size, i, output_dir)
-		#cell_imgs.append(cell)
-	#return cell_imgs
-	return
+	cell_imgs = []
+	for index, data in segs.iterrows():
+		cell = crop_img(img, data[x_center_col], data[y_center_col], box_size, index, output_dir)
+		cell_imgs.append(cell)
+	return cell_imgs
 
-def prep_dino(img:tiff, coord_dir, numx:int, numy:int, nuclei_channel:int, boxsize:int, output_dir: str):
-	'''Crops image to an image for each cell.
-
-	inputs:
-	img -- str -- path to tiff image
-	coord_dir -- str -- path to directory that contains CellSeg coordinate output files.
-	numx -- int -- number of tiles to divide image into on x-axis
-	numy -- number of tiles to divide image into on y-axis (for example, if numx=4 and numy=2 the image will be cropped into tiles by a 4x2 grid)
-	nuclei_channel -- int -- image channel that includes nuclei staining
-	boxsize -- int -- desired boxsize x boxsize size of cropped cell images
-	output_dir -- str -- path to output directory for cell crops
-	'''
-
-	tif = OrigTiff(img,numx,numy, nuclei_channel)
-	xbounds, ybounds = tif.get_bounds()
-	files = tif.get_tile_names()
-	coords = merge_coords(coord_dir, files,  xbounds, ybounds, "Absolute X", "Absolute Y", output_dir)
-	img_to_cells(img, coords, boxsize, output_dir)
-
+def prep_scDino(img, num_x, num_y, box_size, output_dir="./cell_segs"):
+	coords = merge_coords(img, num_x, num_y)
+	cell_imgs = img_to_cells(img, coords, "X", "Y", box_size, output_dir)
+	return cell_imgs
